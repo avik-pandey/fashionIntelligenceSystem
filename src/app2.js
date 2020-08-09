872863ab2905d50f3623c29c08ca531bb86b5ab5
Avik Pandey <avikpandey1@gmail.com>
2020-08-09 22:57:10 +0530

    ok

--- src/app2.js
+++ src/app2.js
@@ -24,7 +24,7 @@ var vogue = mongoose.Schema({
 
 }, { collection: 'magazine' });
 
-var vogue = mongoose.model("magazine", magazine);
+var vogue = mongoose.model("vogue", vogue);
 
 
 const app = express()
--- src/routers/instaHashtag-route.js
+++ src/routers/instaHashtag-route.js
@@ -1,9 +1,103 @@
 const express = require('express');
 const insta = require('../models/insta.js')
+var flipkart = require('../models/flipkart.js');
 const router = new express.Router()
-
+var categories = ['upper body','lower body','full body'];
 router.get('/vogueIndia', (req, res) => {
-    res.render('vogueIndia');
+    res.render('vogueIndia',{ categories,tShirtsMen, majCatFin ,subCatMenFin ,subCatWomenFin});
+})
+
+var allItems = [];
+var majCat = [];
+var allWomenItems = [];
+var subCatWomen = [];
+var allMenItems = [];
+var subCatMen = [];
+var majCatFin = [];
+var subCatMenFin = [];
+var subCatWomenFin = [];
+
+var tShirtsMen = [];
+router.get('/vogueupper%20body', (req, res) => {
+
+    
+
+    async function f1(req, rep) {
+
+        let temp = await flipkart.find({}, function (err, collection) {
+            if (err) console.log(err);
+
+            allItems = collection;
+            for (var i = 0; i < allItems.length; i++) {
+                var fake = allItems[i].majorCategory;
+                majCat.push(fake);
+            }
+
+            majCatFin = Array.from(new Set(majCat));
+
+        });
+        if (temp.err) {
+            console.log(err)
+        }
+        else {
+            console.log("1");
+        }
+
+        let temp1 = await flipkart.find({ majorCategory: 'Men' }, function (err, collection) {
+            if (err)
+                console.log(err)
+            allMenItems = collection;
+
+            for (var i = 0; i < allMenItems.length; i++) {
+                var fake = allMenItems[i].subCategory
+                subCatMen.push(fake);
+            }
+
+            subCatMenFin = Array.from(new Set(subCatMen));
+
+        });
+        if (temp1.err) {
+            console.log(err)
+        }
+        else {
+
+        }
+
+        let temp2 = await flipkart.find({ majorCategory: 'Women' }, function (err, collection) {
+            if (err)
+                console.log(err)
+            allWomenItems = collection;
+
+            for (var i = 0; i < allWomenItems.length; i++) {
+                var fake = allWomenItems[i].subCategory
+                subCatWomen.push(fake);
+            }
+
+            subCatWomenFin = Array.from(new Set(subCatWomen));
+            footwearMen = []; modelsFin = [];
+        });
+        if (temp2.err) {
+            console.log(err)
+        }
+        else {
+
+        }
+
+        let temp3 = await flipkart.find({ subCategory: "T-Shirts" }, function (err, collection) {
+            if (err) console.log(err)
+            tShirtsMen = collection;
+            modelsFin = [];
+        })
+        if (temp3.err) {
+            console.log(err)
+        }
+        else {
+            console.log(tShirtsMen);
+            res.render("vogueIndia", { categories,tShirtsMen, majCatFin ,subCatMenFin ,subCatWomenFin  
+            });
+        }
+    }
+    f1();
 })
 var vogue2020 = [],elle2020 = [],fashion = [];
 router.get('/instaHashtags', (req, res) => {
